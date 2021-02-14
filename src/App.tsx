18db5c6f150964a419b32d9a7d9f9225a6d1cd31
Rajkumar <myidrajkumar@gmail.com>
2021-02-14 21:54:07 +0530

    Adding fake apis to get data with async

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { useQuery } from "react-query";
+
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+const getProducts = async ():Promise<CartItemType[]> => 
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
 const App = () => {
+
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
+  
+
   return (
     <div className="App">
       <h1>
